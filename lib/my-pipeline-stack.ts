7cc0b88215d87c4961937506dbd75d0d101864c7
aruni.tillekeratne <aruni.tillekeratne@musala.com>
2026-01-12 23:48:46 +1100

    added lambda

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { SecretValue } from 'aws-cdk-lib';
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -17,5 +18,9 @@ export class MyPipelineStack extends cdk.Stack {
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
+
+    pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "009087543814", region: "us-west-2" }
+    }));
   }
 }
