5112782a9006313c3347aac56a6e260f37d3226f
aruni.tillekeratne <aruni.tillekeratne@musala.com>
2026-01-12 23:43:05 +1100

    added secret

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { SecretValue } from 'aws-cdk-lib';
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,7 +10,10 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('arunit9/my-pipeline', 'main'),
+        input: CodePipelineSource.gitHub('arunit9/my-pipeline', 'main',{
+      // By default, looks for a secret named 'github-token'
+      authentication: SecretValue.secretsManager('github-token'),
+        }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
